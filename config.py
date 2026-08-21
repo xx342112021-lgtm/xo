b27ae4dc6b4eb6fa871eb8b3be95b4378b617a4c
xx342112021-lgtm <xx342112021@gmail.com>
2026-08-21 09:19:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,25 +5,25 @@ load_dotenv()
 
 class Config:
     def __init__(self):
-        self.API_ID = int(getenv("API_ID", 0))
-        self.API_HASH = getenv("API_HASH")
+        self.API_ID = int(getenv("API_ID", 39170545))
+        self.API_HASH = getenv("API_HASH","65959028b3bddfd808d52d0f93749bc0")
 
-        self.BOT_TOKEN = getenv("BOT_TOKEN")
-        self.MONGO_URL = getenv("MONGO_URL")
+        self.BOT_TOKEN = getenv("BOT_TOKEN","8628186514:AAFru9OFAFdsRa3Lze31CA3jzPDrv8UajCE")
+        self.MONGO_URL = getenv("MONGO_URL","mongodb+srv://Che3434:Che3434@cluster0.oemtavn.mongodb.net/?appName=Cluster0")
 
-        self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
-        self.OWNER_ID = list(map(int, getenv("OWNER_ID", "").split(",")))
+        self.LOGGER_ID = int(getenv("LOGGER_ID", -1003712681521))
+        self.OWNER_ID = list(map(int, getenv("OWNER_ID", "8745446616").split(",")))
 
         self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60)) * 60
         self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 20))
         self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", 20))
         self.STICKER_ID = getenv("STICKER_ID", "")
-        self.SESSION1 = getenv("SESSION", None)
+        self.SESSION1 = getenv("SESSION", "BAJVsfEAjln36gI4_N8S-QoXWRlNzAKrC88014MfiBvcSORRMc9x_B04zMs5LaWwIrguVqpvMH9fSdKsOIlwg7ab0dpu9Ucg5_CU6KNwnxtaOJ-fzY0xM0BYvwr7I054SoxlwEtoBJruiu_RE6IGIBSWBIqizUlRxO9ln6GdFLAW2dVfN5t6-iqPeKnppoICYxUp5MfDABQRpQeuLwqXG0mICjFTL91IxcqJvrRutiYAkrJJil0A0d61b16XgNxuPwQRqrDskpyg_mipsvg4ldLL8zPjr-sh5Sa_22UUe7c6gKkSlBMK5zMVkSGf60LdclUZYH7EofiPsMIhjp4h69qgCvlF9AAAAAII_dZ9AA")
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Alfabots_update")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Alfabots_support")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/destekkkk4")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/destekkkk4")
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
         self.AUTO_END: bool = getenv("AUTO_END", "False").lower() == "true"
