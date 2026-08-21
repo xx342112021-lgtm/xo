69edf934b2d6f290937fea6a887dff7a28262f35
xx342112021-lgtm <xx342112021@gmail.com>
2026-08-21 09:23:21 +0300

    Update pause.py

--- opus/plugins/pause.py
+++ opus/plugins/pause.py
@@ -9,7 +9,7 @@ from opus import anon, app, db, lang
 from opus.helpers import buttons, can_manage_vc
 
 
-@app.on_message(filters.command(["pause"]) & filters.group & ~app.bl_users)
+@app.on_message(filters.command(["pause","dur","durdur"]) & filters.group & ~app.bl_users)
 @lang.language()
 @can_manage_vc
 async def _pause(_, m: types.Message):
